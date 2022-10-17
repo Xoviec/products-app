89dc9bc8ffa22eb10533495c58e0f60a7afb455e
Adrian Gąsiorek <adriangasiorek@MacBook-Pro-Adrian.local>
2022-10-17 12:41:33 +0200

    added brand select sorting

--- src/componentss/AssetBrandSelect.tsx
+++ src/componentss/AssetBrandSelect.tsx
@@ -1,8 +1,12 @@
 import { Menu } from "@headlessui/react"
+import { useState } from "react"
 import Dropdown from "./Dropdown"
 
 export const AssetBrandSelect = ({catBrand, setCatBrand}) =>{
 
+
+    const [brands, setBrands] = useState(['Apple', 'Samsung','HP Pavilion','OPPO','Microsoft Surface','Infinix','Impression of Acqua Di Gio','Royal_Mirage','Fog Scent Xpressio'])
+ 
     const handleBrandChange = (event) =>{
         event.target.innerText == 'All' ? setCatBrand('') :
         setCatBrand(event.target.innerText.toUpperCase())
@@ -18,57 +22,16 @@ export const AssetBrandSelect = ({catBrand, setCatBrand}) =>{
                     <button onClick={handleBrandChange}>All
                         <div className="w-[100%] bg-[#3c83ec] h-[1px] opacity-50 mt-2"> </div>
                     </button>
-        
-                </Menu.Item>
-                <Menu.Item class='text-left text-[#3c83ec] hover:text-[#FBFDFF] hover:bg-[#3c83ec] pl-3 pt-2 duration-100'>
-                    <button onClick={handleBrandChange}>Apple
-                        <div className="w-[100%] bg-[#3c83ec] h-[1px] opacity-50 mt-2"> </div>
-                    </button>
-                </Menu.Item>
-                <Menu.Item class='text-left text-[#3c83ec] hover:text-[#FBFDFF] hover:bg-[#3c83ec] pl-3 pt-2 duration-100'>
-                    <button onClick={handleBrandChange}>Samsung
-                        <div className="w-[100%] bg-[#3c83ec] h-[1px] opacity-50 mt-2"> </div>
-                    </button>
+            </Menu.Item>
+            {
+                 brands.sort().map((brands)=>
+                 <Menu.Item class='text-left text-[#3c83ec] hover:text-[#FBFDFF] hover:bg-[#3c83ec] pl-3 pt-2 duration-100'>
+                 <button onClick={handleBrandChange}>{brands}
+                     <div className="w-[100%] bg-[#3c83ec] h-[1px] opacity-50 mt-2"> </div>
+                 </button>
                 </Menu.Item >    
-                <Menu.Item class='text-left text-[#3c83ec] hover:text-[#FBFDFF] hover:bg-[#3c83ec] pl-3 pt-2 duration-100'>
-                    <button onClick={handleBrandChange}>HP Pavilion
-                        <div className="w-[100%] bg-[#3c83ec] h-[1px] opacity-50 mt-2"> </div>
-                    </button>
-                </Menu.Item>    
-                <Menu.Item class='text-left text-[#3c83ec] hover:text-[#FBFDFF] hover:bg-[#3c83ec] pl-3 pt-2 duration-100'>
-                    <button onClick={handleBrandChange}>Huawei
-                        <div className="w-[100%] bg-[#3c83ec] h-[1px] opacity-50 mt-2"> </div>
-                    </button>
-                </Menu.Item>
-                <Menu.Item class='text-left text-[#3c83ec] hover:text-[#FBFDFF] hover:bg-[#3c83ec] pl-3 pt-2 duration-100'>
-                    <button onClick={handleBrandChange}>OPPO
-                        <div className="w-[100%] bg-[#3c83ec] h-[1px] opacity-50 mt-2"> </div>
-                    </button>
-                </Menu.Item>  
-                <Menu.Item class='text-left text-[#3c83ec] hover:text-[#FBFDFF] hover:bg-[#3c83ec] pl-3 pt-2 duration-100'>
-                    <button onClick={handleBrandChange}>Microsoft Surface
-                        <div className="w-[100%] bg-[#3c83ec] h-[1px] opacity-50 mt-2"> </div>
-                    </button>
-                </Menu.Item>     
-                <Menu.Item class='text-left text-[#3c83ec] hover:text-[#FBFDFF] hover:bg-[#3c83ec] pl-3 pt-2 duration-100'>
-                    <button onClick={handleBrandChange}>Infinix
-                        <div className="w-[100%] bg-[#3c83ec] h-[1px] opacity-50 mt-2"> </div>
-                    </button>
-                </Menu.Item>            
-                <Menu.Item class='text-left text-[#3c83ec] hover:text-[#FBFDFF] hover:bg-[#3c83ec] pl-3 pt-2 duration-100'>
-                    <button onClick={handleBrandChange}>Impression of Acqua Di Gio
-                        <div className="w-[100%] bg-[#3c83ec] h-[1px] opacity-50 mt-2"> </div>
-                    </button>
-                </Menu.Item>     
-                <Menu.Item class='text-left text-[#3c83ec] hover:text-[#FBFDFF] hover:bg-[#3c83ec] pl-3 pt-2 duration-100'>
-                    <button onClick={handleBrandChange}>Royal_Mirage
-                        <div className="w-[100%] bg-[#3c83ec] h-[1px] opacity-50 mt-2"> </div>
-                    </button>
-                </Menu.Item>    
-                <Menu.Item class='text-left text-[#3c83ec] hover:text-[#FBFDFF] hover:bg-[#3c83ec] pl-3 pt-2 pb-2 duration-100'>
-                    <button onClick={handleBrandChange}>Fog Scent Xpressio
-                    </button>
-                </Menu.Item>     
+                 )
+            }
             </Dropdown>
       
         </div>
